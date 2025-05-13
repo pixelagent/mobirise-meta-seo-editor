a12ce868740d30931370f19fcd8edcd088422ab0
Purna <purna@users.noreply.github.com>
2025-05-13 16:49:42 +0100

    new angle

--- src/app.js
+++ src/app.js
@@ -2,39 +2,35 @@ defineM("meta-seo-editor", function(jQuery, mbrApp, TR) {
   mbrApp.regExtension({
     name: "meta-seo-editor",
     events: {
-      load: function() {
+      load: function () {
         console.log("meta-seo loaded");
-        // Set variable for this
-        var a = this;
-        // Add a new panel to the sidebar
-        a.addFilter("sidebarPageSettings", function(settingsPanels, pageData) {
+        const a = this;
+
+        a.addFilter("sidebarPageSettings", function (settingsPanels, pageData) {
           const val = pageData["meta-seo-value"] || "";
 
           const html = [
+            '<form class="mbr-form">',
             '<div class="form-group col-md-12">',
             '  <label class="control-label">Meta Description</label>',
             '  <input type="text" name="meta-seo-input" class="form-control" value="' + val + '" placeholder="Enter meta description">',
-            '</div>'
+            '</div>',
+            '</form>'
           ].join("\n");
-          // Setting panel
+
           settingsPanels.push({
             title: "Meta SEO",
             name: "meta-seo-editor",
             html: html
           });
 
-          console.log("Injected meta SEO panel for", pageData.filename);
-
           return settingsPanels;
         });
 
-        // Save value on change
         mbrApp.$body.on("input", "input[name='meta-seo-input']", function () {
           const page = mbrApp.activePage;
-          // Check if the page is a valid page
           if (page) {
             page["meta-seo-value"] = $(this).val().trim();
-            console.log("Saved meta SEO value:", page["meta-seo-value"]);
           }
         });
       }
